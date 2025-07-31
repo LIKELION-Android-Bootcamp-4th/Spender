f7abad863af51e433fc660da914e6d380bd273b3
DD <jah06114@naver.com>
2025-07-31 18:10:38 +0900

    feat: 앱 전역에서 사용하는 폰트 패밀리와 테마 추가

--- Spender/app/src/main/java/com/example/spender/feature/home/HomeScreen.kt
+++ Spender/app/src/main/java/com/example/spender/feature/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.spender.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -12,13 +13,26 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.spender.core.ui.MyTypography
 
 @Composable
 fun HomeScreen(navHostController: NavHostController) {
-    Box(modifier = Modifier
+    Column(modifier = Modifier
         .fillMaxSize()
         .background(MaterialTheme.colorScheme.background)
     ) {
-        Text("HomeScreen", fontSize = 50.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+        Text("titleLarge", style = MyTypography.titleLarge)
+        Text("titleMedium", style = MyTypography.titleMedium)
+        Text("titleSmall", style = MyTypography.titleSmall)
+
+        Text("bodyLarge", style = MyTypography.bodyLarge)
+        Text("bodyMedium", style = MyTypography.bodyMedium)
+        Text("bodySmall", style = MyTypography.bodySmall)
+
+        Text("labelLarge", style = MyTypography.labelLarge)
+        Text("labelMedium", style = MyTypography.labelMedium)
+        Text("labelSmall", style = MyTypography.labelSmall)
+
+
     }
 }
