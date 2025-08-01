806c18e5bd89e953f9458a14f76572b34a6b0444
suji lee <soojilee3579@naver.com>
2025-08-01 15:21:04 +0900

    Feat: 리포트 피드백 ui 완료

--- Spender/app/src/main/java/com/example/spender/feature/report/ui/detail/component/CategoryPieChart.kt
+++ Spender/app/src/main/java/com/example/spender/feature/report/ui/detail/component/CategoryPieChart.kt
@@ -1,5 +1,6 @@
 package com.example.spender.feature.report.ui.detail.component
 
+import android.graphics.Typeface
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
@@ -9,24 +10,15 @@ import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import android.graphics.Color as AndroidColor
 import androidx.core.graphics.toColorInt
+import com.github.mikephil.charting.formatter.ValueFormatter
 
 
 @Composable
-fun CategoryPieChart() {
-    val entries = listOf(
-        PieEntry(61f, "식비"),
-        PieEntry(21f, "자녀/육아"),
-        PieEntry(14f, "교통"),
-        PieEntry(3f, "교육/학습")
-    )
-
-    val colors = listOf(
-        "#18C1AC".toColorInt(), // 식비
-        "#2E9AFE".toColorInt(), // 자녀/육아
-        "#8E44AD".toColorInt(), // 교통
-        "#9B59B6".toColorInt()  // 교육/학습
-    )
-
+fun CategoryPieChart(
+    labels: List<String>,
+    values: List<Float>,
+    colors: List<Int>
+) {
     AndroidView(factory = { context ->
         PieChart(context).apply {
             layoutParams = ViewGroup.LayoutParams(
@@ -41,10 +33,23 @@ fun CategoryPieChart() {
             legend.isEnabled = false
         }
     }, update = { chart ->
-        val dataSet = PieDataSet(entries, "")
-        dataSet.colors = colors
-        dataSet.valueTextSize = 14f
-        dataSet.valueTextColor = android.graphics.Color.WHITE
+        val maxIndex = values.indices.maxBy { values[it] }
+
+        val entries = values.mapIndexed { index, value ->
+//            if (index == maxIndex) {
+//                PieEntry(value, labels[index])
+//            } else {
+//                PieEntry(value, "")
+//            }
+            PieEntry(value, labels[index])
+        }
+
+        val dataSet = PieDataSet(entries, "").apply {
+            setColors(colors)
+            valueTextSize = 0f
+            valueTextColor = AndroidColor.WHITE
+            valueTypeface = Typeface.DEFAULT_BOLD
+        }
 
         val data = PieData(dataSet)
         chart.data = data
--- Spender/app/src/main/java/com/example/spender/feature/report/ui/detail/component/FeedbackSection.kt
+++ Spender/app/src/main/java/com/example/spender/feature/report/ui/detail/component/FeedbackSection.kt
@@ -1,12 +1,25 @@
 package com.example.spender.feature.report.ui.detail.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.spender.R
 import com.example.spender.ui.theme.Typography
 
 @Composable
@@ -19,5 +32,53 @@ fun FeedbackSection(){
             style = Typography.titleMedium
         )
 
+        Spacer(modifier = Modifier.height(12.dp))
+
+        FeedbackBox(
+            text = "이번 달은 외식비가 많았어요.\n다음 달에는 줄여볼까요?",
+            imageResId = R.drawable.spender
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
+
+@Composable
+fun FeedbackBox(
+    text: String,
+    imageResId: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = Color.Gray,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 피드백
+        Text(
+            text = text,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp),
+            style = Typography.bodyMedium
+        )
+
+        // 지출이
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = "캐릭터",
+            modifier = Modifier
+                .size(64.dp)
+        )
     }
 }
