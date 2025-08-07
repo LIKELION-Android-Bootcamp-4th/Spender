138dbefe4ea9c2c00b18772eb0503aa399116da2
DD <jah06114@naver.com>
2025-08-07 13:00:58 +0900

    refactor: Naver Init 위치 변경(메인->어플리케이션)

--- Spender/app/src/main/java/com/example/spender/MyApplication.kt
+++ Spender/app/src/main/java/com/example/spender/MyApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -12,6 +13,13 @@ class MyApplication: Application(){
         super.onCreate()
 
         KakaoSdk.init(this, getString(R.string.kakao_app_key))
+
+        NaverIdLoginSDK.initialize(
+            context = this,
+            clientId = "btOnE17uFYFcVX7H36Tm",
+            clientSecret = "lVwABQpwoF",
+            clientName = "Spender"
+        )
 
 //        var keyHash = Utility.getKeyHash(this)
 //        Log.i("GlobalApplication", "$keyHash")
