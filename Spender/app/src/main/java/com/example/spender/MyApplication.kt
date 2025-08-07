ea1d5a829eac664f82d6a7f6f08f1c2233d8e72f
DD <jah06114@naver.com>
2025-08-07 12:35:23 +0900

    feat: 카카오 로그인 구현

--- Spender/app/src/main/java/com/example/spender/MyApplication.kt
+++ Spender/app/src/main/java/com/example/spender/MyApplication.kt
@@ -1,7 +1,19 @@
 package com.example.spender
 
 import android.app.Application
+import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication: Application()
+class MyApplication: Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, getString(R.string.kakao_app_key))
+
+//        var keyHash = Utility.getKeyHash(this)
+//        Log.i("GlobalApplication", "$keyHash")
+    }
+}
--- Spender/app/src/main/java/com/example/spender/feature/auth/domain/AuthRepository.kt
+++ Spender/app/src/main/java/com/example/spender/feature/auth/domain/AuthRepository.kt
@@ -3,29 +3,37 @@ package com.example.spender.feature.auth.domain
 import android.util.Log
 import com.example.spender.feature.auth.data.FirebaseAuthDataSource
 import com.example.spender.feature.auth.data.NaverDataSource
+import com.example.spender.feature.auth.data.KakaoDataSource
 import kotlinx.coroutines.tasks.await
 
 class AuthRepository(
     private val firebaseAuthDataSource: FirebaseAuthDataSource,
-    private val naverDataSource: NaverDataSource
+    private val naverDataSource: NaverDataSource,
+    private val kakaoDataSource: KakaoDataSource
 ) {
 
     suspend fun naverLogin() {
         val accessToken = naverDataSource.signIn()
-        Log.d("AuthRepository", "naver token : $accessToken")
+        Log.d("Login", "naver token : $accessToken")
         signInWithCustomToken("naverCustomAuth", accessToken)
+    }
+
+    suspend fun kakaoLogin() {
+        val accessToken = kakaoDataSource.signIn()
+        Log.d("Login", "kakao token : $accessToken")
+        signInWithCustomToken("kakaoCustomAuth", accessToken)
     }
 
     private suspend fun signInWithCustomToken(functionName: String, accessToken: String) {
         val result = firebaseAuthDataSource.requestCustomToken(functionName, accessToken).await()
         val customToken = (result.data as Map<*, *>)["token"] as String
 
-        Log.d("AuthRepository", "signInWithCustomToken token : $customToken")
+        Log.d("Login", "signInWithCustomToken token : $customToken")
         try {
             val authResult = firebaseAuthDataSource.signInWithCustomToken(customToken).await()
-            Log.d("AuthRepository", "signInWithCustomToken success: ${authResult.user?.uid}")
+            Log.d("Login", "signInWithCustomToken success: ${authResult.user?.uid}")
         } catch (e: Exception) {
-            Log.e("AuthRepository", "signInWithCustomToken failed", e)
+            Log.e("Login", "signInWithCustomToken failed", e)
             throw e
         }
     }
