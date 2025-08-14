c911a9de15044ed385fde0213581cc5c50945489
suji lee <soojilee3579@naver.com>
2025-08-14 16:26:54 +0900

    WIP: 바로가기 하는중

--- Spender/app/src/main/java/com/example/spender/core/widget/SpenderMiniReceiverWidget.kt
+++
@@ -1,8 +0,0 @@
-package com.example.spender.core.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class SpenderMiniReceiverWidget : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = SpenderMiniWidget()
-}
