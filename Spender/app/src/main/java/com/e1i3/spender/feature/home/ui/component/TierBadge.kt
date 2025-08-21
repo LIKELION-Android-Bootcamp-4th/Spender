1f3aabceed7b532c03b36b6e2b491b3c85762cba
eunaj99 <esfjge@gmail.com>
2025-08-21 20:26:34 +0900

    fix: 티어 뱃지 디자인 수정

--- Spender/app/src/main/java/com/e1i3/spender/feature/home/ui/component/TierBadge.kt
+++ Spender/app/src/main/java/com/e1i3/spender/feature/home/ui/component/TierBadge.kt
@@ -1,12 +1,19 @@
 package com.e1i3.spender.feature.home.ui.component
 
+import android.graphics.Paint.Align
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +25,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.e1i3.spender.R
+import com.e1i3.spender.ui.theme.LightPointColor
 import com.e1i3.spender.ui.theme.PointColor
+import com.e1i3.spender.ui.theme.SelectedColor
+import com.e1i3.spender.ui.theme.Typography
 
 @Composable
 fun TierBadge(
@@ -31,21 +41,33 @@ fun TierBadge(
 ) {
     val tierDrawable = tierDrawableRes(level)
 
-    Box(
-        modifier = modifier
-            .size(size)
-            .shadow(elevation, CircleShape, clip = false)
-            .clip(CircleShape)
-            .background(pointColor),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            painter = painterResource(id = tierDrawable),
-            contentDescription = contentDescription,
+    Box(modifier = modifier.width(size+50.dp)) {
+        Box(
+            modifier = modifier
+                .size(size)
+                .shadow(elevation, CircleShape, clip = false)
+                .background(pointColor, CircleShape)
+                .align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(id = tierDrawable),
+                contentDescription = contentDescription,
+                modifier = Modifier
+                    .fillMaxSize(0.72f)
+                    .align(Alignment.Center)
+                    .offset(x = -(3).dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+        Box(
             modifier = Modifier
-                .fillMaxSize(0.72f),
-            contentScale = ContentScale.Fit
-        )
+                .size(75.dp)
+                .background(SelectedColor, CircleShape)
+                .align(Alignment.TopEnd),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(contentDescription ?: "", style = Typography.bodyMedium, color = Color.Black)
+        }
     }
 }
 
