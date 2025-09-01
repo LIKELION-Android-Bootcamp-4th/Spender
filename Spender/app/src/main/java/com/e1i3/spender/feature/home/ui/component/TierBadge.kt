e1994306f89fdd85580cce21422d0e034767e1f0
jah06114 <jah06114@naver.com>
2025-09-01 11:38:22 +0900

    fix: 홈화면 티어 클릭이벤트 제거

--- Spender/app/src/main/java/com/e1i3/spender/feature/home/ui/component/TierBadge.kt
+++ Spender/app/src/main/java/com/e1i3/spender/feature/home/ui/component/TierBadge.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -50,7 +52,11 @@ fun TierBadge(
     Box(
         modifier = modifier
             .width(size + 50.dp)
-            .clickable(onClick = onClick),
+            .clickable(
+                onClick = onClick,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ),
     ) {
         Box(
             modifier = modifier
